dcc8efdc01cb96fdd7f45ec858a48552f2d8cbc7
IniOluwa <inioluwafageyinbo@gmail.com>
2017-03-03 15:42:58 +0100

    Basically configured.

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+  <head>
+    <title>ACADA!</title>
+  </head>
+  <body>
+    <header></header>
+    <section></section>
+    <footer></footer>
+  </body>
+</html>
