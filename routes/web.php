c46aaab55408d1ba22edb5a63197adde40b721ce
IniOluwa <inioluwafageyinbo@gmail.com>
2017-03-05 06:07:57 +0100

    Laravel welcome incommented.

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 Route::get('/', function () {
   return view('landing');
